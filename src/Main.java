f68223c94c201934f786475d864d0513ee221828
will9191 <willian.fernando040@gmail.com>
2024-03-07 01:27:30 -0300

    finished project

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+       final JFrame frame = new JFrame("Snake Game");
+        frame.setSize(WIDTH, HEIGHT);
+        SnakeGame game = new SnakeGame(WIDTH, HEIGHT);
+        frame.add(game);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.pack();
+        game.startGame();
     }
 }
